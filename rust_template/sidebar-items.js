66de7e38d2507aef37037aa4b5198a5e7e400b72
neoncitylights <sam.t.nguyenn@gmail.com>
2022-09-11 01:25:31 +0000

    Deploying to gh-pages from @ nlp-rs/rust-template@1b4128c938519ed7119d3376bc90ddc1ec097c27 🚀

---
+++ rust_template/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["greet","Rust Repository Template 🦀"]]};
---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"rust_template":{"doc":"","t":[5],"n":["greet"],"q":["rust_template"],"d":["Rust Repository Template 🦀"],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
